36f8f12b86a622b0e8f11475df9eab8d467878d5
Geory <1003491960@qq.com>
2013-07-08 14:22:50 +0800

    no message

---
+++ MeetingCloud/MeetingCloud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetingCloud
+//
+//  Created by songhang he on 12-6-8.
+//  Copyright (c) 2012年 ffcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navController;
+
+@end
